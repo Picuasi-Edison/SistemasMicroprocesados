fbbf647b8b54c214180dc1c431d79337d5924c0d
EdisonPicuasi <f3rch1095@gmail.com>
2019-04-30 11:50:44 -0500

    inicio

---
+++ EdisonPicuasi-Deber2/EdisonPicuasi-Deber2.ino
@@ -0,0 +1,47 @@
+//Edison Picuasi
+//Deber 2
+
+int bt1=7;
+int bt2=6;
+int bt3=5;
+int A=13;
+int B=12;
+int C=11;
+int D=10;
+int dec=9;
+int uni=8;
+void setup() {
+
+}
+
+void loop() {
+
+}
+
+void numeros (int j){
+  
+  switch(j){
+    case 0:
+    break;
+    case 1:
+    break;
+    case 2:
+    break;
+    case 3:
+    break;
+    case 4:
+    break;
+    case 5:
+    break;
+    case 6:
+    break;
+    case 7:
+    break;
+    case 8:
+    break;
+    case 9:
+    break;
+    
+    }
+  
+  }
