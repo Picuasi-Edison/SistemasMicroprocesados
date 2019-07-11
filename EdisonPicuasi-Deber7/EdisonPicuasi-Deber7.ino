d57b756d8f6660799148f2144bcd963c95dfbbac
EdisonPicuasi <f3rch1095@gmail.com>
2019-07-11 12:45:26 -0500

    Reloj con Alarma almacenda en EEPROM

--- EdisonPicuasi-Deber7/EdisonPicuasi-Deber7.ino
+++ EdisonPicuasi-Deber7/EdisonPicuasi-Deber7.ino
@@ -13,36 +13,37 @@ Realice un sistema reloj con alarma almacenada en la EEPROM.
 
 LiquidCrystal lcd(13,12,11,10,9,8); //declara y asigna los pines del lcd
 
-int segundo, minuto, hora;
-int aminuto,ahora;
-int opcion,estado;
-String CX;
-String pass="ok";
-String msg;
+int segundo, minuto, hora;  //variables minuto hora y segundo para el reloj
+int aminuto,ahora;  //variable minuto y hora para la alarma
+int opcion,estado;  //variable de monitoreo
+String CX;  //variable de validacion contrasena
+String pass="ok"; //variable contrasena
+String msg; //variable para el mensaje
 void setup() {
-Serial.begin(9600);
-lcd.begin(16,2);
-segundo=EEPROM.read(0);
-minuto=EEPROM.read(1);
-hora=EEPROM.read(2);
-aminuto=EEPROM.read(3);
-ahora=EEPROM.read(4);
-pinMode(7,OUTPUT);
-Serial.print("Minuto: ");
-Serial.println(EEPROM.read(3));
-Serial.print("Hora: ");
-Serial.println(EEPROM.read(4));
+Serial.begin(9600); //inicializa CX Serial
+lcd.begin(16,2);  //inicializa lcd
+segundo=EEPROM.read(0); //almacena el dato de la memoria EEPROM en la variable
+minuto=EEPROM.read(1);  //almacena el dato de la memoria EEPROM en la variable
+hora=EEPROM.read(2);  //almacena el dato de la memoria EEPROM en la variable
+aminuto=EEPROM.read(3); //almacena el dato de la memoria EEPROM en la variable
+ahora=EEPROM.read(4); //almacena el dato de la memoria EEPROM en la variable
+pinMode(7,OUTPUT);  //configura pin del buzzer
+Serial.print("Minuto: "); //imprime el mensaje por CX
+Serial.println(EEPROM.read(3));   //lee el dato almacenado en EEPROM
+Serial.print("Hora: "); //imprime por CX el mensaje
+Serial.println(EEPROM.read(4)); //lee el dato almacenado en EEPROM
 
 /*
+ * proceso para borrar datos en la memoria EEPROM
 for(int i=0;i<7;i++){
   EEPROM.write(i,0);
  }
  
 */
-MsTimer2::set(1000,reloj);
-MsTimer2::start();
-attachInterrupt(0,curser,FALLING);
-attachInterrupt(1,modificar,FALLING);
+MsTimer2::set(1000,reloj);  //configura y establece el tiempo
+MsTimer2::start();  //inicia el timer
+attachInterrupt(0,curser,FALLING);  //configuracion interrupcion
+attachInterrupt(1,modificar,FALLING); //configuracion interrupcion
 }
 
 void loop() {
@@ -67,13 +68,13 @@ if(Serial.available()>0){
 
 if(opcion==1){
   
-  if(EEPROM.read(1)==EEPROM.read(3)&&EEPROM.read(2)==EEPROM.read(4)){
+  if(EEPROM.read(1)==EEPROM.read(3)&&EEPROM.read(2)==EEPROM.read(4)){ //verifica si las variables de la alarma y reloj es igual
     
-    digitalWrite(7,HIGH);
+    digitalWrite(7,HIGH);//Buzzer
     }
   }else{
     
-    digitalWrite(7,LOW);
+    digitalWrite(7,LOW);//Buzzer
     
     }
 
@@ -82,22 +83,22 @@ if(opcion==1){
 
 void reloj (){
   
-if(segundo<59){
-  segundo++;
+if(segundo<59){ //comprueba si el valor de segundo es menor a 59
+  segundo++;  //incrementa variable
   }else{
-    segundo=0;
-    if(minuto<59){
-      minuto++;
-      EEPROM.update(1,minuto);
+    segundo=0;  //reinicia variable
+    if(minuto<59){  //comprueba si el valor de minuto es menor a 59
+      minuto++; //incrementa variable
+      EEPROM.update(1,minuto);  //almacena el dato en la memoria EEPROM
       }else{
-        EEPROM.update(1,minuto);
-        minuto=0;
-        if(hora<24){
-          hora++;
-          EEPROM.update(2,hora);
+        EEPROM.update(1,minuto);  //almacena el dato en la memoria EEPROM
+        minuto=0; //reinicia variable
+        if(hora<24){  //comprueba si el valor de hora es menor a 24
+          hora++; //incrementa variable
+          EEPROM.update(2,hora);  //almacena el dato en la memoria EEPROM
           }else{
-            EEPROM.update(2,hora);
-            hora=0;
+            EEPROM.update(2,hora);  //almacena el dato en la memoria EEPROM
+            hora=0; //reinicia variable
             }
         }
     }
@@ -155,45 +156,44 @@ if(segundo<59){
 }
 
 void curser(){
-  estado++;
+  estado++; //incrementa variable
 switch(estado){
   case 1:
-  Serial.println("Alarma minuto");
+  Serial.println("Alarma minuto");  //imprime el mensaje por CX Serial
   break;
 
   case 2:
-  Serial.println("Alarma hora");
+  Serial.println("Alarma hora");  //Imprime el mensaje por CX serial
   break;
   default:
-  estado=0;
+  estado=0; //reinicia la variable
   }
   }
 
 void modificar(){
-if(estado==1){
-  if(aminuto<59){
-    
-    aminuto++;
-      Serial.println(aminuto);
-      EEPROM.update(3,aminuto);
+if(estado==1){  //verifica el estado actual
+  if(aminuto<59){ //verifica si la variable es menor a 59
+    aminuto++;  //incrementa la variable
+      Serial.println(aminuto);  //Imprime por CX serial
+      EEPROM.update(3,aminuto); //almacena el dato en la EEPROM
     }else{
-      aminuto=0;
-      Serial.println(aminuto);
-      EEPROM.update(3,aminuto);
+      aminuto=0;  //reinicia variable
+      Serial.println(aminuto);  //Imprime por CX serial
+      EEPROM.update(3,aminuto); //almacena el dato en la EEPROM
       }
   }
 
-if(estado==2){
+if(estado==2){  //verifica el estado actual
   
- if(ahora<24){
+ if(ahora<24){  //verifica si la variable es menor a 24
     
-    ahora++;
-      Serial.println(ahora);
-      EEPROM.update(4,ahora);
+    ahora++;  //incrementa la variable
+      Serial.println(ahora);  //Imprime por CX serial
+      EEPROM.update(4,ahora); //almacena el dato en la EEPROM
     }else{
       ahora=0;
-      Serial.println(ahora);
-      EEPROM.update(4,ahora);
+      Serial.println(ahora);  //Imprime por CX serial
+      EEPROM.update(4,ahora); //almacena el dato en la EEPROM
       }
   }
 }
